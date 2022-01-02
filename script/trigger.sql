9d415a22f4ddf6606fb7f968709717e4632f8137
LinhCoderHcmus <92260801+LinhCoderHcmus@users.noreply.github.com>
2022-01-02 22:15:37 +0700

    Update trigger.sql

--- script/trigger.sql
+++ script/trigger.sql
@@ -1,12 +1,6 @@
 ﻿--Trigger
 -- Số lượng đơn hàng
-UPDATE CUSTOMER  
-SET TotalBills = 
-(select 
-count(*) from Bill o 
-where o.Customer_ID = Customer.Customer_ID
-)
-go
+
 
 create or alter trigger TotalBills on Bill
 for insert,delete 
@@ -26,11 +20,7 @@ go
 
 -- Số tiền đã mua
 
-update Customer set
-TotalMoney = (select sum(BILL_TOTAL_COST) from BILL o 
-where o.CUSTOMER_ID=Customer.CUSTOMER_ID
-group by o.CUSTOMER_ID)
-go
+
 
 create or alter trigger TotalMoney on Bill_Detail
 for insert,delete
@@ -43,22 +33,11 @@ where Customer_ID  in ( select Customer_ID from Inserted union select Customer_I
 
 
 --Giá
-UPDATE 
-BILL_DETAIL
-SET PRICE =(SELECT PRICE FROM BOOK B
-WHERE B.BOOK_ID= BILL_DETAIL.BOOK_ID)
+
 
 
 -- Tổng tiền 
-UPDATE dbo.Bill
-SET Bill_Total_Cost = f.valsum
-FROM 
-(
-  SELECT BD.Bill_ID,  SUM(BD.PRICE * BD.Quantity) valsum
-  FROM dbo.Bill_Detail BD 
-  GROUP BY BD.BILL_ID
-) f
-WHERE dbo.BILL.BILL_ID= f.BILL_ID
+
 
 go 
 
@@ -79,13 +58,38 @@ end
 
 go 
 
-UPDATE BOOK
-SET TOTAL_SOLD_BOOK =
+create or alter trigger Import_ToTal_Cost on Imported_Bill_Detail
+for insert,delete 
+as 
+begin
+UPDATE dbo.Imported_bill
+SET Total_Cost = f.valsum
+FROM 
 (
-SELECT COUNT(QUANTITY) FROM CART C
-WHERE C.BOOK_ID=BOOK.BOOK_ID
-GROUP BY BOOK_ID
-)
+  SELECT BD.Ip_ID,  SUM(BD.Imported_PRICE * BD.Quantity) valsum
+  FROM dbo.Imported_Bill_Detail BD 
+  GROUP BY BD.IP_ID
+) f
+WHERE dbo.Imported_bill.Ip_ID= f.Ip_ID
+and  Imported_bill.Ip_ID IN (SELECT  Ip_ID FROM Inserted UNION SELECT Ip_ID FROM Deleted)
+end
+
+go
+
+create or alter trigger Export_ToTal_Cost on Exported_Bill_Detail
+for insert,delete 
+as 
+begin
+UPDATE dbo.Exported_bill
+SET Total_Cost = f.valsum
+FROM 
+(
+  SELECT ex.Exported_bill_id,  SUM(b.price * ex.Quantity) valsum
+  FROM dbo.Exported_Bill_Detail ex  join book b on ex.book_id=b.book_id
+  GROUP BY ex.Exported_bill_id
+) f
+WHERE dbo.Exported_bill.Exported_id= f.Exported_bill_id and Exported_bill.Exported_id in ( SELECT  Exported_bill_id FROM Inserted UNION SELECT Exported_bill_id FROM Deleted)
+end
 
 go
 create or alter trigger Total_Sold_Book on Bill_Detail
@@ -94,7 +98,7 @@ as
 UPDATE BOOK
 SET TOTAL_SOLD_BOOK =
 (
-SELECT COUNT(QUANTITY) FROM CART C
+SELECT COUNT(QUANTITY) FROM CART_DETAIL C
 WHERE C.BOOK_ID=BOOK.BOOK_ID
 GROUP BY BOOK_ID
 )
