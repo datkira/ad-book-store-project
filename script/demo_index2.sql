fd4fb92073fca6a86a83fc59a8c6ca28af9f04a9
LinhCoderHcmus <92260801+LinhCoderHcmus@users.noreply.github.com>
2022-01-02 22:06:28 +0700

    Xóa

--- script/demo_index2.sql
+++
@@ -1,9 +0,0 @@
-﻿--Truy vấn 3: Đối với những đơn hàng của khách hàng cụ thể, lấy ra thông tin về tổng tiền của đơn hàng    
-SELECT BI.BILL_ID, BI.CREATED_DATE, BI.BILL_TOTAL_COST
-FROM BILL BI, CUSTOMER C
-WHERE BI.CUSTOMER_ID = C.CUSTOMER_ID AND C.CUSTOMER_ID = 1
-
---Truy vấn 4: Thống kê doanh thu theo ngày 
-SELECT SUM(BILL_TOTAL_COST)
-FROM BILL  
-WHERE CREATED_DATE = '2021-3-12'
